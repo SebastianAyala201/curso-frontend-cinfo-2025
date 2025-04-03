58390c7e42450d6f171be9f997ca83f0131a3374
SebastianAyala201 <sebastianayala201@gmail.com>
2025-04-03 13:10:34 -0500

    Aprendiendo mas de local storage

--- sesion_04/content/views/importacion_de_productos.js
+++ sesion_04/content/views/importacion_de_productos.js
@@ -10,8 +10,34 @@ document.getElementById("guardarBtn").addEventListener("click", function () {
     console.log("RUC: ", ruc);
     console.log("Moneda de pago: ", mon);
 
-    localStorage.setItem("Nombre de proveedor: ", prov);
-    localStorage.setItem("RUC: ", ruc);
-    localStorage.setItem("Moneda de pago: ", mon);
+    //Guardar datos
+    localStorage.setItem("nomProv", prov);
+    localStorage.setItem("ruc", ruc);
+    localStorage.setItem("monPago", mon);
+
+    //Obtener datos
+    let nombreLocalStorage = localStorage.getItem("nomProv");
+    //Pasar local storage a alert
+    alert("Nombre Local Storage: "+ nombreLocalStorage)
+
+    //Obtener datos
+    let rucLocalStorage = localStorage.getItem("ruc");
+    //Pasar local storage a Html
+    document.getElementById("message").innerText += "\n" + rucLocalStorage;
+
+    // Eliminar Ruc desde localStorage
+    localStorage.removeItem("ruc");
+    alert("ruc eliminado: " + localStorage.getItem("ruc"));
 });
 
+document.getElementById("retornarBtn").addEventListener("click", function () {
+    document.getElementById("MensajesDiv").innerHTML = "<strong>Retornando a inicio</strong>";
+
+    setTimeout(function () {
+        window.location.href = "index.html";// Realiza la redirección
+    }, 3000);// Espera el valor, expresado en milisegundos, luego ejecuta el código de la función anónima.
+});
+
+window.addEventListener("load",function(){
+    alert("Cargando pagina...")
+})
